b895a75927df8507b419d4a29416e8811f33b64e
Ryan Swann <RyanSwann1992@gmail.com>
2020-11-10 14:36:57 +0000

    Platforms now spawn Enemy Triggers and Enemy spawn positions

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -8,37 +8,54 @@ public class Platform : MonoBehaviour
     [SerializeField]
     private GameObject m_spawnIndicatorPrefab;
     [SerializeField]
-    private float m_xOffsetFromEdge;
+    private GameObject m_enemyTriggerPrefab;
     [SerializeField]
-    private float m_yOffsetFromPlatform;
-    private GameObject m_leftSpawn;
-    private GameObject m_rightSpawn;
+    private float m_enemySpawnXOffsetFromEdge;
+    [SerializeField]
+    private float m_enemySpawnYOffsetFromPlatform;
+    [SerializeField]
+    private float m_enemyTriggerXOffsetFromEdge;
+    private GameObject m_leftEnemySpawn;
+    private GameObject m_rightEnemySpawn;
+    private GameObject m_leftEnemyTrigger;
+    private GameObject m_rightEnemyTrigger;
+    [SerializeField]
+    private bool m_enemyInteractable;
 
     void Awake()
     {
-        m_leftSpawn = Instantiate(m_spawnIndicatorPrefab, new Vector3(transform.position.x + transform.localScale.x / 2.0f - m_xOffsetFromEdge,
-            transform.position.y + m_yOffsetFromPlatform, transform.position.z), Quaternion.identity);
+        Assert.IsNotNull(m_spawnIndicatorPrefab);
+        Assert.IsNotNull(m_enemyTriggerPrefab);
 
-        m_rightSpawn = Instantiate(m_spawnIndicatorPrefab, new Vector3(transform.position.x - transform.localScale.x / 2.0f + m_xOffsetFromEdge,
-            transform.position.y + m_yOffsetFromPlatform, transform.position.z), Quaternion.identity);
+        if(m_enemyInteractable)
+        {
+            m_leftEnemySpawn = Instantiate(m_spawnIndicatorPrefab, new Vector3(transform.position.x + transform.localScale.x / 2.0f - m_enemySpawnXOffsetFromEdge,
+                transform.position.y + m_enemySpawnYOffsetFromPlatform, transform.position.z), Quaternion.identity);
+
+            m_rightEnemySpawn = Instantiate(m_spawnIndicatorPrefab, new Vector3(transform.position.x - transform.localScale.x / 2.0f + m_enemySpawnXOffsetFromEdge,
+                transform.position.y + m_enemySpawnYOffsetFromPlatform, transform.position.z), Quaternion.identity);
+
+            m_leftEnemyTrigger = Instantiate(m_enemyTriggerPrefab, new Vector3(transform.position.x + transform.localScale.x / 2.0f - m_enemyTriggerXOffsetFromEdge,
+                transform.position.y + m_enemySpawnYOffsetFromPlatform, transform.position.z), Quaternion.identity);
+
+            m_rightEnemyTrigger = Instantiate(m_enemyTriggerPrefab, new Vector3(transform.position.x - transform.localScale.x / 2.0f + m_enemyTriggerXOffsetFromEdge,
+                transform.position.y + m_enemySpawnYOffsetFromPlatform, transform.position.z), Quaternion.identity);
+        }
     }
 
     // Start is called before the first frame update
     void Start()
-    {}
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        m_leftSpawn.transform.position = new Vector3(transform.position.x + transform.localScale.x / 2.0f - m_xOffsetFromEdge,
-            transform.position.y + m_yOffsetFromPlatform, transform.position.z);
-
-        m_rightSpawn.transform.position = new Vector3(transform.position.x - transform.localScale.x / 2.0f + m_xOffsetFromEdge,
-            transform.position.y + m_yOffsetFromPlatform, transform.position.z);
     }
 
     public GameObject getRandomSpawn()
     {
-        return Random.Range(0, 1) == 0 ? m_leftSpawn : m_rightSpawn;
+        return Random.Range(0, 1) == 0 ? m_leftEnemySpawn : m_rightEnemySpawn;
     }
 }
