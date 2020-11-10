a88ed35cecfc6608f996f795b11df06bd633d9a9
raccoonwarlord <caden.galavan@gmail.com>
2020-11-10 01:25:13 +0000

    Implementing more enemy code
    Slowly adding more code for the different enemies

--- Assets/Change_Position.cs
+++ Assets/Change_Position.cs
@@ -4,47 +4,68 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 public class Change_Position : MonoBehaviour
-{
+{ 
 	//Position
 	public GameObject Point1;
 	public GameObject Point2;
 	int Target_Point = 0;
-	float Position_Timer;
+	public float Position_Timer;
 
 	//Movement
-	public float Movement_Speed = 2f;
+	//public float Movement_Speed = 2f;
 	public GameObject Player;
 	public bool Activate;
 
+	[SerializeField]
+	private float m_movementSpeed;
+	private Rigidbody m_rigidBody;
+
+	private void Awake()
+	{
+		m_rigidBody = GetComponent<Rigidbody>();
+		Assert.IsNotNull(m_rigidBody);
+		m_rigidBody.velocity = new Vector3(m_movementSpeed, 0.0f, 0.0f);
+	}
 
     // Start is called before the first frame update
     void Start()
     {
-		Position_Timer = 2f;
+		//Position_Timer = 2f;
 		this.gameObject.transform.position = new Vector3 (Point2.transform.position.x, Point2.transform.position.y, Point2.transform.position.z);
 
     }
 
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("ChangeEnemyDirection"))
+		{
+			m_rigidBody.velocity = -m_rigidBody.velocity;    
+		}
+	}
+
     // Update is called once per frame
     void Update()
     {
-		if ((this.gameObject.transform.position - Player.transform.position).sqrMagnitude < 50f){
 
-			Activate = true;
-		
-		}
-		else{
-			Activate = false;
-		}
+		transform.position += m_rigidBody.velocity * m_movementSpeed * Time.deltaTime;
 
-		if (Activate == true){
+//		if ((this.gameObject.transform.position - Player.transform.position).sqrMagnitude < 100f){
+//
+//			Activate = true;
+//		
+//		}
+//		else{
+//			Activate = false;
+//		}
+//
+//		if (Activate == true){
 		Position_Timer -= Time.deltaTime;
 
 		if (Position_Timer < 0){
 			Change_Point();
 			Position_Timer = 2f;
 		}
-		}
+//		}
 
 		
     }
