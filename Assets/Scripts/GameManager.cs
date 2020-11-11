2ef8f79447084af84f29810a1be13b8cc952b9b8
Ryan Swann <RyanSwann1992@gmail.com>
2020-11-11 14:42:43 +0000

    Initial GameManager implementation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_levelEndGameObject;
+    private int m_pickupQuantity;
+
+    void Awake()
+    {
+        Assert.IsNotNull(m_levelEndGameObject);
+        m_levelEndGameObject.gameObject.SetActive(false);
+        m_pickupQuantity = GameObject.FindGameObjectsWithTag("PickUp").Length;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onPlayerPickupCollection(int playerPickupQuantity)
+    {
+        Assert.IsTrue(playerPickupQuantity <= m_pickupQuantity);
+        if(playerPickupQuantity == m_pickupQuantity)
+        {
+            m_levelEndGameObject.SetActive(true);
+        }
+    }
+}
